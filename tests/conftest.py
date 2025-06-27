ed1d23209ca60f7c31430533ee718cb7d0a9c65b
Анна Краснокутская <anna.kharina.1998@mail.ru>
2025-06-27 10:21:11 +0300

    добавлена параметризация

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,28 @@ from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
-
+# from dotenv import load_dotenv
 from tests.utils import attach
+
+DEFAULT_BROWSER_VERSION = "128.0"
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='128.0'
+    )
+
+
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
     # options = Options()
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = webdriver.ChromeOptions()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "128.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True,
